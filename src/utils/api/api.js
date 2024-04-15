8a0d6b48ab0c2ae4ac218ad7b451e1b0e0cfd3ed
Ilya Bondarchuk <cod3provider@gmail.com>
2024-04-15 12:19:26 +0200

    added: api for requests by id

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,9 +1,20 @@
 import axios from "axios";
 
+const PER_PAGE = 4;
+
 axios.defaults.baseURL = 'https://640f0b414ed25579dc43f4ef.mockapi.io/api/campers';
+axios.defaults.params = {
+  limit: PER_PAGE,
+  page: 1,
+}
 
 export const getCampers = async () => {
-  const {data} = await axios();
-  console.log(data)
+  const {data} = await axios("/");
   return data;
+}
+
+export const getCamperById = async(id) => {
+  const res = await axios(`/${id}`);
+  console.log(res);
+  return res;
 }
