70e7153508ee89bd796f241ae1e4d6b5dda26b7c
Ilya Bondarchuk <cod3provider@gmail.com>
2024-04-16 19:19:19 +0200

    refactored api for redux

--- src/utils/api/api.js
+++ src/utils/api/api.js
@@ -1,20 +1,6 @@
 import axios from "axios";
 
-export const PER_PAGE = 4;
+export const instance = axios.create({
+  baseURL: 'https://640f0b414ed25579dc43f4ef.mockapi.io/api/campers',
+})
 
-axios.defaults.baseURL = 'https://640f0b414ed25579dc43f4ef.mockapi.io/api/campers';
-axios.defaults.params = {
-  limit: PER_PAGE,
-}
-
-export const getCampers = async (page = 1) => {
-  const {data} = await axios("/");
-  console.log(data)
-  return data;
-}
-
-export const getCamperById = async(id) => {
-  const {data} = await axios(`/${id}`);
-  console.log(data);
-  return {data};
-}
