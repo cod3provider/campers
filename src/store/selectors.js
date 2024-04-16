2c1c108c75e31ca0fd0875a50fa860dfc549f021
Ilya Bondarchuk <cod3provider@gmail.com>
2024-04-16 19:19:54 +0200

    added selectors

---
+++ src/store/selectors.js
@@ -0,0 +1,5 @@
+export const selectCampers = state => state.campers.items;
+export const selectFavorites = state => state.campers.favorites;
+export const selectError = state => state.campers.error;
+export const isLoading = state => state.campers.isLoading;
+export const selectFilter = state => state.filter;
