ea3ae3009deb1c79fb9e3155e63e753e50c0db5b
Ilya Bondarchuk <cod3provider@gmail.com>
2024-04-24 22:33:53 +0200

    added Datepicker

--- src/components/Datepicker/Datepicker.jsx
+++ src/components/Datepicker/Datepicker.jsx
@@ -1,76 +1,44 @@
-import React from 'react';
-import { DatePicker, Space } from 'antd';
-import dayjs from 'dayjs';
-// const { RangePicker } = DatePicker;
+import React, {useRef} from "react";
+import { DatePicker, Space } from "antd";
+import dayjs from "dayjs";
 
-import s from './Datepicker.module.scss';
+import s from "./Datepicker.module.scss";
 
-const onChange = (date) => {
-	if (date) {
-		console.log('Date: ', date);
-	} else {
-		console.log('Clear');
-	}
-};
-// const onRangeChange = (dates, dateStrings) => {
-// 	if (dates) {
-// 		console.log('From: ', dates[0], ', to: ', dates[1]);
-// 		console.log('From: ', dateStrings[0], ', to: ', dateStrings[1]);
-// 	} else {
-// 		console.log('Clear');
-// 	}
-// };
-// const rangePresets = [
-// 	{
-// 		label: 'Last 7 Days',
-// 		value: [dayjs().add(-7, 'd'), dayjs()],
-// 	},
-// 	{
-// 		label: 'Last 14 Days',
-// 		value: [dayjs().add(-14, 'd'), dayjs()],
-// 	},
-// 	{
-// 		label: 'Last 30 Days',
-// 		value: [dayjs().add(-30, 'd'), dayjs()],
-// 	},
-// 	{
-// 		label: 'Last 90 Days',
-// 		value: [dayjs().add(-90, 'd'), dayjs()],
-// 	},
-// ];
-const Datepicker = () => (
+const Datepicker = ({ onChange }) => {
+	const datePickerRef = useRef(null);
+
+	const disabledDate = (current) => {
+		return current && current < dayjs().endOf('day');
+	};
+
+	return (
 	<Space direction="vertical" size={12}>
 		<DatePicker
+			disabledDate={disabledDate}
+			ref={datePickerRef}
 			className={s.datepicker}
 			presets={[
 				{
-					label: 'Tomorrow',
-					value: dayjs().add(+1, 'd'),
+					label: "Tomorrow",
+					value: dayjs().add(+1, "d"),
 				},
 				{
-					label: 'Next Week',
-					value: dayjs().add(+7, 'd'),
+					label: "Next Week",
+					value: dayjs().add(+7, "d"),
 				},
 				{
-					label: 'Next Month',
-					value: dayjs().add(+1, 'month'),
+					label: "Next Month",
+					value: dayjs().add(+1, "month"),
 				},
 			]}
-			onChange={onChange}
+			onChange={(date, dateString) => {
+				if (dateString) {
+					onChange(dateString);
+				} else {
+					onChange("");
+				}
+			}}
 		/>
-		{/*<RangePicker presets={rangePresets} onChange={onRangeChange} />*/}
-		{/*<RangePicker*/}
-		{/*	presets={[*/}
-		{/*		{*/}
-		{/*			label: <span aria-label="Current Time to End of Day">Now ~ EOD</span>,*/}
-		{/*			value: () => [dayjs(), dayjs().endOf('day')], // 5.8.0+ support function*/}
-		{/*		},*/}
-		{/*		...rangePresets,*/}
-		{/*	]}*/}
-		{/*	showTime*/}
-		{/*	format="YYYY/MM/DD HH:mm:ss"*/}
-		{/*	onChange={onRangeChange}*/}
-		{/*/>*/}
-	</Space>
-);
+	</Space>)
+};
 export default Datepicker;
