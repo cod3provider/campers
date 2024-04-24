71136600feccdf9acdc74cf3f9c2e398063eb1b9
Ilya Bondarchuk <cod3provider@gmail.com>
2024-04-24 22:29:59 +0200

    refactored Catalog

--- src/pages/Catalog/Catalog.jsx
+++ src/pages/Catalog/Catalog.jsx
@@ -1,12 +1,17 @@
-import Container from "../../components/common/Container/Container.jsx";
 import { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import Container from "../../components/common/Container/Container.jsx";
+import Button from "@/components/common/Button/Button.jsx";
 import CardsList from "../../components/CardsList/CardsList.jsx";
 import Filter from "@/components/Filter/Filter.jsx";
-import { useDispatch, useSelector } from "react-redux";
+
 import { selectCampers, selectPage } from "@/store/selectors.js";
 import { fetchCampers } from "@/store/operations.js";
 import { setFilter } from "@/store/filter/filterSlice.js";
 import { setPage } from "@/store/campers/campersSlice.js";
+
+import s from "./Catalog.module.scss";
+import Section from "@/components/common/Section/Section.jsx";
 
 const Catalog = (props) => {
   const dispatch = useDispatch();
@@ -30,12 +35,17 @@ const Catalog = (props) => {
 
   return (
     <Container>
-      Catalog
-      {/*<aside>*/}
-      <Filter />
-      {/*</aside>*/}
-      <CardsList campers={campers} />
-      {isShowBtn && <button onClick={handleClick}>Load more</button>}
+      <Section className={s.section}>
+        {/*<aside>*/}
+        <Filter />
+        {/*</aside>*/}
+        <div className={s.wrap}>
+          <CardsList campers={campers} />
+          {isShowBtn && (
+            <Button className={s.btn} text="Load more" onClick={handleClick} />
+          )}
+        </div>
+      </Section>
     </Container>
   );
 };
